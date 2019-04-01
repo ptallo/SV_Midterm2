aa23003cc1ba5bb5efe1b3b927314541bfc154b7
S Lazarus <noreply>
2019-04-01 16:23:22 -0400

    Add ipheader.py script

---
+++ ipheader.py
@@ -0,0 +1,148 @@
+# Script for sending a IPv4 packet with a custom header
+# Retrieved from http://www.bitforestinfo.com/2017/12/code-to-create-ipv4-packet-header-in-python.html
+
+import socket
+
+class IPPacket:
+ def __init__(self, dst='127.0.0.1', src='192.168.1.101'):
+  self.dst = dst
+  self.src = src
+  self.raw = None
+  self.create_ipv4_feilds_list()
+
+ def create_ipv4_feilds_list(self):
+
+  # ---- [Internet Protocol Version] ----
+  ip_ver = 4
+  ip_vhl = 5
+
+  self.ip_ver = (ip_ver << 4 ) + ip_vhl
+
+  # ---- [ Differentiate Servic Field ]
+  ip_dsc = 0
+  ip_ecn = 0
+
+  self.ip_dfc = (ip_dsc << 2 ) + ip_ecn
+
+  # ---- [ Total Length]
+  self.ip_tol = 0
+
+  # ---- [ Identification ]
+  self.ip_idf = 54321
+
+  # ---- [ Flags ]
+  ip_rsv = 0
+  ip_dtf = 0
+  ip_mrf = 0
+  ip_frag_offset = 0
+
+  self.ip_flg = (ip_rsv << 7) + (ip_dtf << 6) + (ip_mrf << 5) + (ip_frag_offset)
+
+  # ---- [ Total Length ]
+  self.ip_ttl = 255
+
+  # ---- [ Protocol ]
+  self.ip_proto = socket.IPPROTO_TCP
+
+  # ---- [ Check Sum ]
+  self.ip_chk = 0
+
+  # ---- [ Source Address ]
+  self.ip_saddr = socket.inet_aton(self.src)
+
+  # ---- [ Destination Address ]
+  self.ip_daddr = socket.inet_aton(self.dst)
+
+  return
+
+ def assemble_ipv4_feilds(self):
+  self.raw = struct.pack('!BBHHHBBH4s4s' , 
+   self.ip_ver,   # IP Version 
+   self.ip_dfc,   # Differentiate Service Feild
+   self.ip_tol,   # Total Length
+   self.ip_idf,   # Identification
+   self.ip_flg,   # Flags
+   self.ip_ttl,   # Time to leave
+   self.ip_proto, # protocol
+   self.ip_chk,   # Checksum
+   self.ip_saddr, # Source IP 
+   self.ip_daddr  # Destination IP
+   )
+  return self.raw
+
+class IPPacket:
+ def __init__(self, dst='127.0.0.1', src='192.168.1.101'):
+  self.dst = dst
+  self.src = src
+  self.raw = None
+  self.create_ipv4_feilds_list()
+
+ def assemble_ipv4_feilds(self):
+  self.raw = struct.pack('!BBHHHBBH4s4s' , 
+   self.ip_ver,   # IP Version 
+   self.ip_dfc,   # Differentiate Service Feild
+   self.ip_tol,   # Total Length
+   self.ip_idf,   # Identification
+   self.ip_flg,   # Flags
+   self.ip_ttl,   # Time to leave
+   self.ip_proto, # protocol
+   self.ip_chk,   # Checksum
+   self.ip_saddr, # Source IP 
+   self.ip_daddr  # Destination IP
+   )
+  return self.raw
+
+
+ def create_ipv4_feilds_list(self):
+
+  # ---- [Internet Protocol Version] ----
+  ip_ver = 4
+  ip_vhl = 5
+
+  self.ip_ver = (ip_ver << 4 ) + ip_vhl
+
+  # ---- [ Differentiate Servic Field ]
+  ip_dsc = 0
+  ip_ecn = 0
+
+  self.ip_dfc = (ip_dsc << 2 ) + ip_ecn
+
+  # ---- [ Total Length]
+  self.ip_tol = 0
+
+  # ---- [ Identification ]
+  self.ip_idf = 54321
+
+  # ---- [ Flags ]
+  ip_rsv = 0
+  ip_dtf = 0
+  ip_mrf = 0
+  ip_frag_offset = 0
+
+  self.ip_flg = (ip_rsv << 7) + (ip_dtf << 6) + (ip_mrf << 5) + (ip_frag_offset)
+
+  # ---- [ Total Length ]
+  self.ip_ttl = 255
+
+  # ---- [ Protocol ]
+  self.ip_proto = socket.IPPROTO_TCP
+
+  # ---- [ Check Sum ]
+  self.ip_chk = 0
+
+  # ---- [ Source Address ]
+  self.ip_saddr = socket.inet_aton(self.src)
+
+  # ---- [ Destination Address ]
+  self.ip_daddr = socket.inet_aton(self.dst)
+
+  return
+
+if __name__=='__main__':
+        # Create Raw Socket
+ s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
+
+ ip = IPPacket()
+ ip.assemble_ipv4_feilds()
+
+ s.sendto(ip.raw, ('127.0.0.1' , 0 ))
