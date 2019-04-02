6673a8bc8e09f8ca42d70c0dcaf990c5a2157c2f
Christopher Ochs <ochscs@mail.uc.edu>
2019-04-02 13:11:52 -0400

    Added some comments

--- scapy_ipheader.py
+++ scapy_ipheader.py
@@ -5,20 +5,27 @@ from scapy.all import *
 
 def main():
     message = "Hello World!"
+    # Create 12 packets
     packets = [IP(dst="github.com") / TCP() for x in range(12)]
     responses = []
 
+    # Encode message into packet ID
     encode_message_in_packets(packets, message)
+
     sys.stdout = open("output/packets.txt", 'w')
+
+    # Send packets, collect responses, display packets
     for p in packets:
         responses.append(sr1(p))
         p.show()
 
     sys.stdout = open("output/responses.txt", 'w')
+    # Display responses
     for i, r in enumerate(responses):
         print("Response {}".format(i))
         r.show()
 
+    # Output the message
     sys.stdout = open("output/message.txt", 'w')
     print(decode_message_from_packets(responses))
 
