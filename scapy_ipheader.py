9a5869ac3fe83c070e680268c52f71cc7994c390
Christopher Ochs <ochscs@mail.uc.edu>
2019-04-02 13:46:53 -0400

    Yo we got that sniffy sniff

--- scapy_ipheader.py
+++ scapy_ipheader.py
@@ -14,9 +14,15 @@ def main():
 
     sys.stdout = open("output/packets.txt", 'w')
 
+    sniffedPackets = sniff(store=1, count=12)
+
     # Send packets, collect responses, display packets
     for p in packets:
         responses.append(sr1(p))
+        p.show()
+
+    sys.stdout = open("output/sniffedPackets.txt", 'w')
+    for p in sniffedPackets:
         p.show()
 
     sys.stdout = open("output/responses.txt", 'w')
@@ -28,6 +34,14 @@ def main():
     # Output the message
     sys.stdout = open("output/message.txt", 'w')
     print(decode_message_from_packets(responses))
+
+
+def sniffPackets(packet):
+    if packet.haslayer(IP):
+        pckt_src = str(packet[IP].src)
+        pckt_dst = str(packet[IP].dst)
+        pckt_ttl = str(packet[IP].ttl)
+        print("IP Packet:" + pckt_src + " is going to " + pckt_dst + " and has ttl value " + pckt_ttl)
 
 
 def encode_message_in_packets(packets, message):
