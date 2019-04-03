62980a94c9f893e0f4ee73bfb595dafa4450e475
Philip Tallo <tallopt@mail.uc.edu>
2019-04-03 16:11:06 -0400

    updated encode/decode

--- scapy_ipheader.py
+++ scapy_ipheader.py
@@ -46,14 +46,17 @@ def sniffPackets(packet):
 
 def encode_message_in_packets(packets, message):
     for i, c in enumerate(message):
-        packets[i][IP].id = encode_character_in_string(packets[i][IP].id, c)
+        new_id = encode_character_in_string(packets[i][IP].id, c)
+        packets[i][IP].id = new_id
 
 
 def encode_character_in_string(string_to_modify, character):
+    if type(string_to_modify) != str:
+        string_to_modify = str(string_to_modify)
     bits_to_encode = string_to_bits(string_to_modify)
     code_bits = string_to_bits(character)
     bits_to_encode[len(bits_to_encode) - len(code_bits):] = code_bits
-    return bits_to_encode
+    return bits_to_string(bits_to_encode)
 
 
 def decode_message_from_packets(packets):
@@ -64,7 +67,19 @@ def decode_message_from_packets(packets):
 
 
 def decode_character_from_packet(p):
-    return "A"
+    id_bits = string_to_bits(p[IP].id)
+    bits_to_decode = id_bits[len(id_bits) - 8:]
+    return bits_to_string(bits_to_decode)
+
+
+def test_encode_decode_from_packets():
+    message = "Hello World!"
+    packets = [IP(dst="github.com") / TCP() for x in range(12)]
+
+    encode_message_in_packets(packets, message)
+    decoded_message = decode_message_from_packets(packets)
+    if message != decoded_message:
+        raise AssertionError("Message not equal to Decoded Message!")
 
 
 def string_to_bits(s):
@@ -85,4 +100,5 @@ def bits_to_string(bits):
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    test_encode_decode_from_packets()
